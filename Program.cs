29171c2b6c831c8610a2188a8e8dd8c7f5b3c776
Datta sonalkar <Dell@DESKTOP-TR6VTDT>
2024-09-25 19:54:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
